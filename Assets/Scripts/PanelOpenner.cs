46fa4728e4c5a0c3e88055e6ab8d43ffd14c4111
картофелина <gabrielle.multan.pro@gmail.com>
2023-02-07 12:40:30 +0100

    UI menu

---
+++ Assets/Scripts/PanelOpenner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpenner : MonoBehaviour
+{
+
+    public GameObject panel;
+
+    public void openPanel()
+    {
+        if(panel != null)
+        {
+
+            bool isActive = panel.activeSelf;
+            panel.SetActive(!isActive);
+        }
+    }
+}

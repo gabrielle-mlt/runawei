4d979088f964ae7115d9cccf708850affb21da23
картофелина <gabrielle.multan.pro@gmail.com>
2023-02-08 13:06:28 +0100

    game over

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour
+{
+    public void changeScene(string sName)
+    {
+        SceneManager.LoadScene(sName);
+
+        // if scene === "map"
+        // reset map
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+}
